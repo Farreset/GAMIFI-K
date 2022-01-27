df8f3e998d57e4a62d1a7c3dc603b7eb0536f36a
Farreset <ericfarrejove@gmail.com>
2022-01-27 19:53:07 +0100

    AAAA
    AAAA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { APP_BASE_HREF } from '@angular/common';
     LoginComponent,
     ProfileProfeComponent,
     ProfileAlumnoComponent
-  ],
+    ],
   imports: [
   BrowserModule,
   ReactiveFormsModule,
@@ -30,6 +30,6 @@ import { APP_BASE_HREF } from '@angular/common';
   Routing
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/'}],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/components/register-alumno/register-alumno.component.ts
+++ src/app/components/register-alumno/register-alumno.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Alumno } from 'src/app/interfaces/interfaz';
+import { ServiceService } from 'src/app/server/service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-alumno',
@@ -6,10 +10,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-alumno.component.css']
 })
 export class RegisterAlumnoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  alumnosArray = [];
+  alumno:FormGroup;
+  ServiceService: any;
+  alumnos:Alumno = {
+    id: 0,
+    nick: "",
+    fname:"" ,
+    lname:"" ,
+    year:"" ,
+    mail:"" ,
+    pssw:""  
+  } 
+  constructor(private formBuilder: FormBuilder, private router: Router, ServiceService: ServiceService){
+    this.formBuilder = formBuilder;
+    this.ServiceService = ServiceService;
+    this.alumno = new FormGroup({
+      nick: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      fname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      lname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      year: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      mail: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      pssw: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    });
+  };
+  ngOnInit() {
+    console.log(this.ServiceService)
   }
 
+  get data() { return this.alumno.controls; }
+
+  onSubmit() {   
+
+    if(this.alumnos.fname.trim().length === 0){
+      return;
+    }
+    if(this.alumnos.lname.trim().length === 0){
+      return;
+    }
+    if(this.alumnos.year.trim().length === 0){
+      return;
+    }
+    if(this.alumnos.mail.trim().length === 0){
+      return;
+    }
+    if(this.alumnos.nick.trim().length === 0){
+      return;
+    }
+    if(this.alumnos.pssw.trim().length === 0){
+      return;
+    }
+ 
+     this.router.navigate(['login', this.alumnos]);
+  }
+  volver(){
+    
+    this.router.navigate(['home']);
+  }
 }
