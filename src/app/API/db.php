bc251724b617118aaa2ad86319ad351939dd5c58
AndresXW5 <73656983+AndresXW5@users.noreply.github.com>
2022-02-03 17:55:44 +0100

    API creada
    Carpeta API creada, con la conexion a la BD y los php de editar el ranking

---
+++ src/app/API/db.php
@@ -0,0 +1,22 @@
+<?php
+
+//Variable global para la conexion con la BD
+global  $enlace;
+
+function conexion() {
+
+  //conexion a la BD
+  $enlace = mysqli_connect('localhost','root','usbw','ajesty')
+    mysqli_set_charset($enlace,"utf8");
+
+    if (!$enlace) {
+      echo "Error: No se puede conectar a MySQL." .PHP_EOL;
+      echo "Error de depuracion: " . mysqli_connect_error() . PHP_EOL;
+      echo "Error de depuracion: " . mysqli_connect_error() . PHP_EOL;
+      exit;
+     }
+    return $enlace;
+
+    }
+
+?>
