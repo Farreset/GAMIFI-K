c4c998c345920a1280ace328b7690795e86b3131
AndresXW5 <73656983+AndresXW5@users.noreply.github.com>
2022-02-03 19:09:42 +0100

    API bien
    Conexion a la bd corregida

--- src/app/API/db.php
+++ src/app/API/db.php
@@ -6,11 +6,11 @@ global  $enlace;
 function conexion() {
 
   //conexion a la BD
-  $enlace = mysqli_connect('localhost','root','usbw','ajesty')
+  $enlace = mysqli_connect('localhost','root','usbw','ajesty');
     mysqli_set_charset($enlace,"utf8");
 
     if (!$enlace) {
-      echo "Error: No se puede conectar a MySQL." .PHP_EOL;
+      echo "Error: No se puede conectar a MySQL." . PHP_EOL;
       echo "Error de depuracion: " . mysqli_connect_error() . PHP_EOL;
       echo "Error de depuracion: " . mysqli_connect_error() . PHP_EOL;
       exit;
@@ -20,3 +20,4 @@ function conexion() {
     }
 
 ?>
+
